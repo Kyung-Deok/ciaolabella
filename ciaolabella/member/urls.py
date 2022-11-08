40a3b19ed417604ca8092a4663668234b19307f2
hanna-joo <wnehfehf@gmail.com>
2022-11-08 14:29:41 +0900

    member_사용자로그경로추가

--- ciaolabella/member/urls.py
+++ ciaolabella/member/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('register/', views.member_reg, name='member_reg'),
     path('login/', views.member_login, name='member_login'),
     path('logout/', views.member_logout, name='member_logout'),
+    path('logout2/', views.member_logout2, name='member_logout2'),
 ]
