569f03023eb420728f8538a0aee3e9a5b67bfd34
nijes <monochrome05@gmail.com>
2022-11-08 12:50:50 +0900

    'edit_ecopoint'

--- ciaolabella/ciaolabella/views.py
+++ ciaolabella/ciaolabella/views.py
@@ -6,16 +6,22 @@ from datetime import datetime
 from member.models import MEMBER,ECOPOINT
 from . import ocr
 import random
+from pymongo import MongoClient
+from PIL import Image
+import gridfs
+from bson.objectid import ObjectId
+import codecs
+from django.core.cache import cache
 
 def index(request): # main
 
     try :
         # 회원 로그 수집
-        rs = MEMBER.objects.filter(id=request.session['row_id']).first()
-        login_logger = logging.getLogger('log')
-        data = {'row_id': rs.id , 'age_nb':rs.age_nb, 'gender_kb ': rs.gender_kb, 'region_kb':rs.region_kb,
-                'log_tm':str(datetime.now()), 'log_kb': 'main'}
-        login_logger.info('menu_log', extra = data)
+        # rs = MEMBER.objects.filter(id=request.session['row_id']).first()
+        # login_logger = logging.getLogger('log')
+        # data = {'row_id': rs.id , 'age_nb':rs.age_nb, 'gender_kb ': rs.gender_kb, 'region_kb':rs.region_kb,
+        #         'log_tm':str(datetime.now()), 'log_kb': 'main'}
+        # login_logger.info('menu_log', extra = data)
 
         return render(request, 'ciaolabella/index.html')
 
@@ -36,11 +42,11 @@ def index(request): # main
 def about(request):
     try :
         # 회원 로그 수집
-        rs = MEMBER.objects.filter(id=request.session['row_id']).first()
-        login_logger = logging.getLogger('log')
-        data = {'row_id': rs.id , 'age_nb':rs.age_nb, 'gender_kb ': rs.gender_kb, 'region_kb':rs.region_kb,
-                'log_tm':str(datetime.now()), 'log_kb': 'about'}
-        login_logger.info('menu_log', extra = data)
+        # rs = MEMBER.objects.filter(id=request.session['row_id']).first()
+        # login_logger = logging.getLogger('log')
+        # data = {'row_id': rs.id , 'age_nb':rs.age_nb, 'gender_kb ': rs.gender_kb, 'region_kb':rs.region_kb,
+        #         'log_tm':str(datetime.now()), 'log_kb': 'about'}
+        # login_logger.info('menu_log', extra = data)
 
         return render(request, 'ciaolabella/about.html')
 
@@ -53,11 +59,11 @@ def aboutecopoint(request):
     if request.method == 'GET':
         try:
             # 로그 수집
-            rs = MEMBER.objects.filter(id=request.session['row_id']).first()
-            login_logger = logging.getLogger('log')
-            data = {'row_id': rs.id , 'age_nb':rs.age_nb, 'gender_kb ': rs.gender_kb, 'region_kb':rs.region_kb,
-                    'log_tm':str(datetime.now()), 'log_kb': 'aboutecopoint'}
-            login_logger.info('menu_log', extra = data)
+            # rs = MEMBER.objects.filter(id=request.session['row_id']).first()
+            # login_logger = logging.getLogger('log')
+            # data = {'row_id': rs.id , 'age_nb':rs.age_nb, 'gender_kb ': rs.gender_kb, 'region_kb':rs.region_kb,
+            #         'log_tm':str(datetime.now()), 'log_kb': 'aboutecopoint'}
+            # login_logger.info('menu_log', extra = data)
             return render(request, 'ecopointapp/aboutecopoint.html')
         except KeyError:
             context = {}
@@ -76,11 +82,11 @@ def ecopoint(request):
             # login_logger.info('menu_log', extra = data)
         try:
             # 로그 수집
-            rs = MEMBER.objects.filter(id=request.session['row_id']).first()
-            login_logger = logging.getLogger('log')
-            data = {'row_id': rs.id , 'age_nb':rs.age_nb, 'gender_kb ': rs.gender_kb, 'region_kb':rs.region_kb,
-                    'log_tm':str(datetime.now()), 'log_kb': 'ecopoint'}
-            login_logger.info('menu_log', extra = data)
+            # rs = MEMBER.objects.filter(id=request.session['row_id']).first()
+            # login_logger = logging.getLogger('log')
+            # data = {'row_id': rs.id , 'age_nb':rs.age_nb, 'gender_kb ': rs.gender_kb, 'region_kb':rs.region_kb,
+            #         'log_tm':str(datetime.now()), 'log_kb': 'ecopoint'}
+            # login_logger.info('menu_log', extra = data)
             return render(request, 'ecopointapp/ecopoint.html')
         except KeyError:
             context = {}
@@ -91,61 +97,105 @@ def ecopoint(request):
         if "image" not in request.FILES:
             return redirect(request.url)
         image = request.FILES['image']
-        # apply image to model
-        upload = {'image': image}
-        url = 'http://127.0.0.1:8001/'
-        resp = requests.post(url, files=upload)
-        result = resp.json() #json 문자열을 파이썬 객체로 변환
-        print(image)
+        member_id = request.session['member_id']
+
+        # 사진 용량 제한
+        if image.size > 16*2**20: # 16MB 초과
+            message = '16MB이하의 사진만 업로드 가능합니다.'
+            return JsonResponse({'msg': message})
+
+        # 사진 메타데이터 인식 및 촬영 날짜 제한
+        try:
+            pic_time = Image.open(image)._getexif()[36867]
+            if pic_time[:10] != datetime.now().strftime("%Y:%m:%d"):
+                message = '오늘 촬영한 사진을 업로드해 주세요.'
+                return JsonResponse({'msg': message})
+        except:
+            message = '사진 촬영시각이 확인되지 않습니다. 사진을 다시 촬영하여 업로드해 주세요.'
+            return JsonResponse({'msg': message})
+
+        # 사진 메타데이터로 하루 2번 제한 검증
+        eco_cache = cache.get(f'{member_id}_1')
+        if eco_cache and eco_cache[0] == datetime.now().strftime("%Y:%m:%d"):
+            if eco_cache[1][0] == 1 and pic_time == eco_cache[1][1]:
+                message = '이미 에코포인트를 적립받은 이미지 입니다.'
+                return JsonResponse({'msg': message})
+            elif eco_cache[1][0] > 1:
+                message = '오늘은 더이상 등록하실 수 없습니다.'
+                return JsonResponse({'msg': message})
+
+        # image mongoDB에 저장
+        client = MongoClient("mongodb://admin:qwer1234@218.154.53.236", 27721)
+        db = client['rawimg']
+        fs = gridfs.GridFS(db)
+        file_id = fs.put(image.read(), member_id=member_id, pic_time=pic_time)
+        #file_id = '63662953b4b738429945f671'
+
+        # Flask와 통신하여 모델 적용
+        data = {'file_id': str(file_id), 'member_id': member_id, 'pic_time': pic_time}
+        url = 'http://3.35.17.248:8001/'
+        try:
+            resp = requests.post(url, data=data)
+            result = resp.json() #json 문자열을 파이썬 객체로 변환
+            modelfile_id = result['modelfile_id']
+            result_list = result['result']
+        except:
+            message = '다른 사진을 업로드 해주세요.'
+            return JsonResponse({'msg': message})
+
+        # mongoDB에서 결과 이미지 로드
+        db = client['ecopoint']
+        fs = gridfs.GridFS(db)
+        img_binary = fs.get(ObjectId(modelfile_id)).read()
+        img_base64 = codecs.encode(img_binary, 'base64')
+        decode_img = img_base64.decode('utf-8')
+
         # ecopoint
-        result_list = result['result']
-        plastic = 0
-        label = 0
-        if f"{image}" == request.session.get("eco_img", None)[0] :
-            message = "중복된 이미지 입니다."
-        elif request.session.get("eco_img", None)[1] >= 2 :
-            if request.session.get("eco_img", None)[2] == datetime.now().strftime("%y-%m-%d"):
-                message = "오늘은 더이상 등록하실 수 없습니다 죄송합니다"
-            else : 
-                request.session.get("eco_img",None)[1] = 0
-        elif result_list:
-            for i in result_list:
-                if i['name'] == 'plastic' and i['confidence'] >= 0.5:
-                    plastic += 1
-                elif i['name'] == 'label' and i['confidence'] >= 0.5:
-                    label += 1
-            if plastic < 1:
-                message = "플라스틱이 아닙니다!"
-            elif plastic >= 1:
-                if label >= 1:
-                    message = f"{label} 개의 불순물이 감지되었습니다. \n깨끗하게 씻어서 분리수거 해주세요!"
-                else:
-                    count = request.session.get("eco_img", 0)[1]
-                    ecopoint = plastic
-                    total_ecopoint = ecopoint*10
-                    message = f'{plastic} 개를 깨끗하게 분리수거하셨네요! \n덕분에 지구가 시원해졌어요! \n{total_ecopoint} ECO POINT 가 적립되었습니다!'
-                    day_count = count + 1
-                    # 에코 포인트 적립
-                        # "ECOPOINT.user_nb" must be a "MEMBER" instance.
-                    ECOPOINT.objects.create(
-                        user_nb=MEMBER.objects.get(id=request.session['row_id']), save_tm = datetime.now(), point_amt=total_ecopoint
+        plastic, label = 0, 0
+        for i in result_list:
+            if i['name'] == 'plastic' and i['confidence'] >= 0.5:
+                plastic += 1
+            elif i['name'] == 'label' and i['confidence'] >= 0.5:
+                label += 1
+        if plastic < 1:
+            message = "플라스틱이 아닙니다!"
+        elif plastic >= 1:
+            if label >= 1:
+                message = f"{label} 개의 불순물이 감지되었습니다. \n깨끗하게 씻어서 분리수거 해주세요!"
+            else:
+                count = 2 if cache.get(f'{member_id}_1') else 1
+                ecopoint = plastic*10
+                message = f'{plastic} 개를 깨끗하게 분리수거하셨네요! \n덕분에 지구가 시원해졌어요! \n{ecopoint} ECO POINT 가 적립되었습니다!'
+                # 에코 포인트 적립
+                month_kb = datetime.now().strftime('%Y-%m')
+                user_ecopoint = ECOPOINT.objects.filter(month_kb=month_kb, member_id=member_id)
+                if user_ecopoint.exists():
+                    org_point = user_ecopoint.first().point_amt
+                    user_ecopoint.update(
+                        point_amt = org_point + ecopoint
                     )
-                    request.session['eco_img'] = [f"{image}", day_count, datetime.now().strftime("%y-%m-%d")]
-                    print(request.session['eco_img'])
-        else:
-            message = '다른 사진을 선택해 주세요.'
-        return JsonResponse({'msg': message})
+                else:
+                    ECOPOINT.objects.create(
+                        member_id = member_id,
+                        month_kb = month_kb,
+                        point_amt = ecopoint
+                    )
+
+                eco_cache = [datetime.now().strftime("%Y:%m:%d"), count, pic_time]
+                cache.set(f'{member_id}_1', eco_cache, 86400)  # 하루 동안 유지
+
+        return JsonResponse({'msg': message, 'decode_img': decode_img})
 
 
 def ecopoint2(request):
     if request.method == 'GET':
         try:
             # 로그 수집
-            rs = MEMBER.objects.filter(id=request.session['row_id']).first()
-            login_logger = logging.getLogger('log')
-            data = {'row_id': rs.id , 'age_nb':rs.age_nb, 'gender_kb ': rs.gender_kb, 'region_kb':rs.region_kb,
-                    'log_tm':str(datetime.now()), 'log_kb': 'ecopoint2'}
-            login_logger.info('menu_log', extra = data)
+            # rs = MEMBER.objects.filter(id=request.session['row_id']).first()
+            # login_logger = logging.getLogger('log')
+            # data = {'row_id': rs.id , 'age_nb':rs.age_nb, 'gender_kb ': rs.gender_kb, 'region_kb':rs.region_kb,
+            #         'log_tm':str(datetime.now()), 'log_kb': 'ecopoint2'}
+            # login_logger.info('menu_log', extra = data)
             return render(request, 'ecopointapp/ecopoint2.html')
         except KeyError:
             context = {}
@@ -156,22 +206,40 @@ def ecopoint2(request):
         if "image" not in request.FILES:
             return redirect(request.url)
 
+        member_id = request.session['member_id']
+
+        # 하루 1번 제한
+        eco_cache = cache.get(f'{member_id}_2')
+        print(f'{member_id}_2', '캐시확인:', eco_cache)
+        if eco_cache and eco_cache[0] == datetime.now().strftime("%Y:%m:%d"):
+            message = '오늘은 이미 에코포인트를 적립받으셨습니다.'
+            return JsonResponse({'msg': message})
+
         image = request.FILES['image']
-                
-        results = ocr.ecopointtwo(image)
+        result = ocr.ecopointtwo(image)
 
-        result = results['images'][0]['fields']
-        list = []
-        for i in result:
-            list.append(i['inferText'])
-        
-        if ('무라벨'or'무라밸'or'라벨X') in list:
+        if ('무라벨'or'무라밸'or'라벨X'or'라밸X'or'라벨x'or'라밸X') in result:
 
-            total_ecopoint = random.randrange(10, 51, 10)
-            message = f"무라벨 제품을 구매하셨군요! \n{total_ecopoint} ECO POINT 가 적립되었습니다!"
-            ECOPOINT.objects.create(
-                        user_nb=MEMBER.objects.get(id=request.session['row_id']), save_tm = datetime.now(), point_amt=total_ecopoint
-            )
+            ecopoint = random.randrange(10, 51, 10)
+            message = f"무라벨 제품을 구매하셨군요! \n{ecopoint} ECO POINT 가 적립되었습니다!"
+
+            # 에코 포인트 적립
+            month_kb = datetime.now().strftime('%Y-%m')
+            user_ecopoint = ECOPOINT.objects.filter(month_kb=month_kb, member_id=member_id)
+            if user_ecopoint.exists():
+                org_point = user_ecopoint.first().point_amt
+                user_ecopoint.update(
+                    point_amt=org_point + ecopoint
+                )
+            else:
+                ECOPOINT.objects.create(
+                    member_id=member_id,
+                    month_kb=month_kb,
+                    point_amt=ecopoint
+                )
+
+            eco_cache = [datetime.now().strftime("%Y:%m:%d")]
+            cache.set(f'{member_id}_2', eco_cache, 86400)  # 하루 동안 유지
 
         else:
             message = '무라벨 제품이 아닙니다'
